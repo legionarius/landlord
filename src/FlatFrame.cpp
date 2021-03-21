cbc1d9b5b4f51630d4f6b2573a7ec59b906e6505
croquelois <croquelois.adrien@gmail.com>
2021-03-21 15:45:39 +0100

    feat: action button condition & icon flat

--- src/FlatFrame.cpp
+++ src/FlatFrame.cpp
@@ -18,6 +18,7 @@ void FlatFrame::_ready() {
 	AudioStreamPlayer *audio = cast_to<AudioStreamPlayer>(get_node("AudioStreamPlayer"));
 	TextureButton *exitButton = cast_to<TextureButton>(get_node("Frame/ExitButton"));
 
+	tenant = nullptr;
 	if(tenant != nullptr) {
 		Ref<PackedScene> tenantIdentityCardScene = ResourceLoader::get_singleton()->load("entity/Tenant/TenantIdentityCard.tscn");
 		TenantIdentityCard *tenantIdentityCard = cast_to<TenantIdentityCard>(tenantIdentityCardScene->instance());
