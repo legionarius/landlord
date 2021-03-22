c1c2f0adfa52e612a2a9fd5585efa17c056331e9
Romain M <romain.mard@gmail.com>
2021-03-22 09:10:21 +0100

    feat: implement light

--- src/FlatIndicators.cpp
+++ src/FlatIndicators.cpp
@@ -13,28 +13,41 @@ void FlatIndicators::_ready() {
 	repairIndicator = cast_to<TextureRect>(get_node("RepairIndicator"));
 	leaveIndicator = cast_to<TextureRect>(get_node("LeaveIndicator"));
 	payedIndicator = cast_to<TextureRect>(get_node("PayedIndicator"));
-
+	notPayedIndicator = cast_to<TextureRect>(get_node("NotPayedIndicator"));
+	indicatorsAnimation = cast_to<AnimationPlayer>(get_node("FlatIndicatorsAnimationPlayer"));
 	repairIndicator->set_visible(false);
 	leaveIndicator->set_visible(false);
 	payedIndicator->set_visible(false);
+	notPayedIndicator->set_visible(false);
 }
 
-void FlatIndicators::_show_repair_indicator() {
-	repairIndicator->set_visible(true);
+void FlatIndicators::_show_indicators() {
+	repairIndicator->set_visible(isRepairVisible);
+	leaveIndicator->set_visible(isLeaveVisible);
+	payedIndicator->set_visible(isPayedVisible);
+	notPayedIndicator->set_visible(isNotPayedVisible);
+	indicatorsAnimation->play("active");
 }
 
-void FlatIndicators::_show_leave_indicator() {
-	leaveIndicator->set_visible(true);
+void FlatIndicators::_hide_indicators() {
+	repairIndicator->set_visible(false);
+	leaveIndicator->set_visible(false);
+	payedIndicator->set_visible(false);
+	notPayedIndicator->set_visible(false);
+	isRepairVisible = false;
+	isLeaveVisible = false;
+	isPayedVisible = false;
+	isNotPayedVisible = false;
 }
 
-void FlatIndicators::_show_payed_indicator() {
-	payedIndicator->set_visible(true);
+void FlatIndicators::_on_FlatIndicatorsAnimationPlayer_animation_finished() {
+	_hide_indicators();
 }
 
-void FlatFrame::_register_methods() {
-	register_method("_init", &FlatFrame::_init);
-	register_method("_ready", &FlatFrame::_ready);
-	register_method("_show_leave_indicator", &FlatFrame::_show_leave_indicator);
-	register_method("_show_payed_indicator", &FlatFrame::_show_payed_indicator);
-	register_method("_show_repair_indicator", &FlatFrame::_show_repair_indicator);
+void FlatIndicators::_register_methods() {
+	register_method("_init", &FlatIndicators::_init);
+	register_method("_ready", &FlatIndicators::_ready);
+	register_method("_show_indicators", &FlatIndicators::_show_indicators);
+	register_method("_hide_indicators", &FlatIndicators::_hide_indicators);
+	register_method("_on_FlatIndicatorsAnimationPlayer_animation_finished", &FlatIndicators::_on_FlatIndicatorsAnimationPlayer_animation_finished);
 }
--- src/FlatIndicators.h
+++ src/FlatIndicators.h
@@ -6,26 +6,33 @@
 #define MUNDANE_JAM_FLATINDICATORS_H
 
 #include <AnimationPlayer.hpp>
+#include <Godot.hpp>
 #include <GridContainer.hpp>
 #include <TextureRect.hpp>
 
 namespace godot {
-class FlatIndicators : public GriContainer {
-	GODOT_CLASS(FlatIndicators, GriContainer);
+class FlatIndicators : public GridContainer {
+	GODOT_CLASS(FlatIndicators, GridContainer);
 
 	AnimationPlayer *indicatorsAnimation;
 	TextureRect *repairIndicator;
 	TextureRect *leaveIndicator;
 	TextureRect *payedIndicator;
+	TextureRect *notPayedIndicator;
 
 public:
 	static void _register_methods();
 	void _init();
 	void _ready();
 
-	void _show_repair_indicator();
-	void _show_leave_indicator();
-	void _show_payed_indicator();
+	bool isRepairVisible = false;
+	bool isLeaveVisible = false;
+	bool isPayedVisible = false;
+	bool isNotPayedVisible = false;
+
+	void _show_indicators();
+	void _hide_indicators();
+	void _on_FlatIndicatorsAnimationPlayer_animation_finished();
 };
 } // namespace godot
 
