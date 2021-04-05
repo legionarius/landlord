cb16cf9803c9031d300d1f1c22de31d7d0fcc171
Romain Marino <romain.mard@gmail.com>
2021-04-05 18:16:46 +0200

    feat: implement popup

---
+++ src/ReportFrame.cpp
@@ -0,0 +1,13 @@
+//
+// Created by nahalu on 01/04/2021.
+//
+
+#include "ReportFrame.h"
+
+using namespace godot;
+
+void ReportFrame::_init() {}
+
+void ReportFrame::_ready() {}
+
+void ReportFrame::_register_methods() {}
